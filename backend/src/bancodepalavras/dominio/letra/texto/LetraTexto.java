dae741d40f84727c76fcdb2a0572c644cf18e3cd
Mateus Nogueira <mateuscnogueira2@gmail.com>
2026-08-26 16:04:18 -0300

    (feat) implement flyweight letter representations

---
+++ backend/src/bancodepalavras/dominio/letra/texto/LetraTexto.java
@@ -0,0 +1,15 @@
+package bancodepalavras.dominio.letra.texto;
+
+import bancodepalavras.dominio.letra.Letra;
+
+public class LetraTexto extends Letra {
+
+    public LetraTexto(char codigo) {
+        super(codigo);
+    }
+
+    @Override
+    public void exibir(Object contexto) {
+        System.out.print(this.getCodigo());
+    }
+}
