9882b082eca1d17ce615109030c1ecbaf2295786
Mateus Nogueira <mateuscnogueira2@gmail.com>
2026-08-25 21:42:48 -0300

    (feat) add base domain object classes

---
+++ backend/src/dominio/ObjetoDominioImpl.java
@@ -0,0 +1,14 @@
+package dominio;
+
+public abstract class ObjetoDominioImpl implements ObjetoDominio {
+    private long id;
+
+    public ObjetoDominioImpl(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+}
